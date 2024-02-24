76124a8ccdbba42cea0fb9efd8af94ada2a65c31
Daniël van de Giessen <daniel@dvdgiessen.nl>
2024-02-24 02:09:12 +0100

    Move executed program logic to DBus caller

--- app/Main.hs
+++ app/Main.hs
@@ -3,27 +3,26 @@
 module Main (main) where
 
 import           Control.Concurrent (threadDelay)
+import           Control.Concurrent.Chan
+import           Control.Exception.Extra (ignore)
 import           Control.Monad
-import           System.Exit
-import           System.Process
 import           DBus.Client
-
-launchProcess :: String -> IO ()
-launchProcess cmd = do
-    (_, _, _, handle) <- createProcess (proc cmd []){std_in = NoStream, std_out = NoStream, std_err = NoStream, close_fds = True, create_group = True, new_session = True}
-    -- TODO: save handle, and then only start new instance after last one closed? also properly close handle so there's no zombie process?
-    return ()
+import           System.Exit
+import           System.Posix.Process
 
 main :: IO ()
 main = do
     -- Set up session
     client <- connectSession
+
+    -- Channel for transfering the process to launch from the callback thread
+    channel <- newChan
     
     -- Export object used for launching programs
-    export client "/Launch" defaultInterface
-             { interfaceName = "nl.dvdgiessen.dbusapplauncher.LaunchProgram"
+    export client "/" defaultInterface
+             { interfaceName = "nl.dvdgiessen.dbusapplauncher.Exec"
              , interfaceMethods =
-               [ autoMethod "Foot" (launchProcess "/usr/bin/foot") ]
+               [ autoMethod "Exec" (\ cmd -> (writeChan channel (cmd, [], Nothing))) ]
              }
 
     -- Connect session to bus
@@ -32,6 +31,22 @@ main = do
         putStrLn "Another service owns the \"nl.dvdgiessen.dbusapplauncher\" bus name"
         exitFailure
 
-    -- Wait forever for method calls
-    forever (threadDelay 50000)
+    -- Retrieve the command to launch from the callback
+    (cmd, args, env) <- readChan channel
+
+    -- Give the thread one millisecond to return its result
+    threadDelay 1000
+
+    -- Disconnect the session
+    unexport client "/Launch"
+    disconnect client
+
+    -- Make sure we are in our own session
+    ignore (do _ <- createSession; return ())
+
+    -- Execute the received command
+    _ <- executeFile cmd True args env
+
+    -- Never reached
+    return ()
 
